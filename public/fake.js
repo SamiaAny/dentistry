96b3f30dc87dd4fe941e8b95f0c69c9c1c40a09a
SamiaAny <nstu.any@gmail.com>
2021-10-18 10:02:08 +0600

    add [react-roter,bootstrap]

---
+++ public/fake.js
@@ -0,0 +1,45 @@
+const services = [
+    {
+        id:1,
+        serviceName: "Dental Implants",
+        description: "A dental implant is a surgical component that interfaces with the bone of the jaw or skull to support a dental prosthesis such as a crown, bridge, denture, or facial prosthesis or to act as an orthodontic anchor.If you have missing or badly damage tooth, this is the solution for you that's why you will regain your natural tooth performance.",
+        imgUrl: "https://i.ibb.co/w4cwQbg/pexels-anna-shvets-3845548.jpg"
+    },
+    {
+        id:2,
+        serviceName: "Laser Dentistry",
+        description: "Dentists use laser for oral surgery.Several variants of dental lasers are in use with different wavelengths and these mean they are better suited for different applications.Dental lasers are not without their benefits, though, as the use of a laser can decrease morbidity after surgery, and reduces the need for anesthetics.",
+        imgUrl: "https://i.ibb.co/xHfz4J3/pexels-andrea-piacquadio-3779711.jpg"
+    },
+    {
+        id:3,
+        serviceName: "Orthodontics",
+        description: "Abnormal alignment of the teeth and jaws is very common.If you have been facing same problem this is the right direction. On average, however, once the braces are put on, they usually remain in place for one to three years. After braces are removed, most patients will need to wear a retainer all the time for the first six months, then only during sleep for many years.",
+        imgUrl: "https://i.ibb.co/X5k2CJR/pexels-cottonbro-6528907.jpg"
+    },
+    {
+        id:4,
+        serviceName: "Endodontics",
+        description: "Root canal treatment is a dental procedure used to treat infected tooth pulp which would be otherwise extracted. This is usually caused when bacteria enter the pulp through a deep cavity or failed filling.It is also done to prevent adverse signs and symptoms from the surrounding sequelae and promote the healing and repair of the surrounding periradicular tissues.",
+        imgUrl: "https://i.ibb.co/PcpgLGn/dentist-gfa7bfae53-640.jpg"
+    },
+    {
+        id:5,
+        serviceName: "Periodontics",
+        description: "Periodontal disease encompasses a number of diseases of the periodontal tissues that result in attachment loss and destruction of alveolar bon.Periodontal disease is the second most common cause of tooth loss.Twice-daily brushing and flossing are a way to help prevent periodontal diseases.",
+        imgUrl: "https://i.ibb.co/3FyH3q0/orthodontist-gfa43d59dc-640.jpg"
+    },
+    {
+        id:6,
+        serviceName: "Cosmetic Dentistry",
+        description: " Cosmetic dentistry is becoming more and more popular, with the industry as a whole projected to reach $32 billion by 2026.This includes include teeth whitening, veneers, fillings, and implants.you can improve the appearance of your mouth, teeth, gums, and overall smile, which is being focuses on professional oral method.",
+        imgUrl: "https://i.ibb.co/ZWJM7CG/pexels-pavel-danilyuk-6812532.jpg"
+    },
+]
+
+// https://i.ibb.co/PcpgLGn/dentist-gfa7bfae53-640.jpg
+// https://i.ibb.co/3FyH3q0/orthodontist-gfa43d59dc-640.jpg
+// https://i.ibb.co/xHfz4J3/pexels-andrea-piacquadio-3779711.jpg
+// https://i.ibb.co/w4cwQbg/pexels-anna-shvets-3845548.jpg
+// https://i.ibb.co/X5k2CJR/pexels-cottonbro-6528907.jpg
+// https://i.ibb.co/ZWJM7CG/pexels-pavel-danilyuk-6812532.jpg

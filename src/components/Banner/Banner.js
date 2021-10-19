5926962bb47b5a5c79a89f54114832a779df623b
SamiaAny <nstu.any@gmail.com>
2021-10-19 19:43:34 +0600

    edit readme.md file and banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -18,8 +18,8 @@ const Banner = () => {
                         />
                     </div>
                     <Carousel.Caption>
-                        <h5>First slide label</h5>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        <h5>Welcome to Dentistry</h5>
+                        <p>Always try to give best treatment</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
@@ -31,8 +31,8 @@ const Banner = () => {
                         />
                     </div>
                     <Carousel.Caption>
-                        <h5>Second slide label</h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <h5>ModernTechnology with Professional Doctors</h5>
+                        <p>Always try to give best treatment</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
@@ -44,8 +44,8 @@ const Banner = () => {
                         />
                     </div>
                     <Carousel.Caption>
-                        <h5>Third slide label</h5>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        <h5>Keep regular checkup for refreshing smile</h5>
+                        <p>Always try to give best treatment</p>
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
